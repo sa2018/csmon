76bc06fb0515c94ead067465d47665ded846495d
SA <sa@sa2018.com>
2017-08-27 21:21:01 +0800

    added default values to args

--- csmon/cli.py
+++ csmon/cli.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser, RawTextHelpFormatter
 from utils.validation import Validation
+from utils.config import Config
 
 
 def run():
@@ -42,18 +43,18 @@ if you are providing URLs as arguments""")
     output_grp_arg("--monitor-log",
                    metavar='FILE',
                    required=False,
-                   default=None,
+                   default=Config.get('LOG_MON_FILE'),
                    type=str,
                    help="[OPTIONAL] Output file to write monitor results.\n"
-                        "[DEFAULT] %s file\n" % None)
+                        "[DEFAULT] %s file\n" % Config.get('LOG_MON_FILE'))
 
     output_grp_arg("--system-log",
                    metavar='FILE',
                    required=False,
                    type=str,
-                   default=None,
+                   default=Config.get('LOG_SYS_FILE'),
                    help="[OPTIONAL] Output file to write system logs.\n"
-                        "[DEFAULT] %s file\n" % None)
+                        "[DEFAULT] %s file\n" % Config.get('LOG_SYS_FILE'))
 
     # Monitoring params
     mon_grp = arg_grp("Monitoring options")
@@ -61,10 +62,10 @@ if you are providing URLs as arguments""")
     mon_grp_arg("--interval",
                 metavar='SECONDS',
                 type=Validation.argtype_positive_integer,
-                default=None,
+                default=Config.get('CHECK_INTERVAL'),
                 help="[OPTIONAL] How many seconds to wait till reschedule the "
                      "next test for the each URL\n"
-                     "DEFAULT] %i seconds\n" % 0)
+                     "DEFAULT] %i seconds\n" % Config.get('CHECK_INTERVAL'))
 
     # HTTP Connection params
     http_grp = arg_grp("Connection options")
@@ -72,27 +73,29 @@ if you are providing URLs as arguments""")
     http_grp_arg("--retry-count",
                  metavar='NUMBER',
                  type=Validation.argtype_unsigned_integer,
-                 default=None,
+                 default=Config.get('CHECK_CONN_MAX_RETRY'),
                  help="[OPTIONAL] Max retry count if service is unavailable"
-                      "[DEFAULT] %i times" % 0)
+                      "[DEFAULT] %i times" % Config.get(
+                     'CHECK_CONN_MAX_RETRY'))
 
     http_grp_arg("--timeout",
                  metavar='SECONDS',
                  type=Validation.argtype_positive_float,
-                 default=None,
+                 default=Config.get('CHECK_CONN_TIMEOUT'),
                  help="[OPTIONAL] Timeout for the connection\n"
-                      "[DEFAULT] %i seconds" % 0)
+                      "[DEFAULT] %i seconds" %
+                      Config.get('CHECK_CONN_TIMEOUT'))
 
     http_grp_arg("--back-off",
                  metavar='NUMBER',
                  type=Validation.argtype_unsigned_float,
-                 default=None,
+                 default=Config.get('CHECK_BACK_OFF_FACTOR'),
                  help="[OPTIONAL] A backoff factor to apply between attempts "
                       "after the second try.\n"
                       "{backoff factor}*(2 ^ ({number of total retries}-1))\n"
                       "If the number is 0.1, then it will sleep for "
                       "[0.0s,0.2s, 0.4s, ...] between retries\n"
-                      "[DEFAULT] %.2f" % 0.00)
+                      "[DEFAULT] %.2f" % Config.get('CHECK_BACK_OFF_FACTOR'))
 
     # Extra validation
     urls = args.urls
