95e67f8b6f8d0c89c67f51d0bc00027f3776621c
SA <sa@sa2018.com>
2017-08-28 00:16:12 +0800

    added utils test files

---
+++ tests/utils/client_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import unittest
+from csmon.utils.client import Client
+
+
+class ClientValidation(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/utils/config_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import unittest
+from csmon.utils.config import Config
+
+
+class ConfigValidation(unittest.TestCase):
+
+    def test_set(self):
+        value = Config.set('CHECK_INTERVAL',50)
+        self.assertEqual(value, Config.get('CHECK_INTERVAL'))
+        with self.assertRaises(NameError):
+            Config.set('ABC', 50)
+
+    def test_get(self):
+        self.assertEqual(0.10, Config.get('CHECK_BACK_OFF_FACTOR'))
+        with self.assertRaises(NameError):
+            Config.get('ABC')
+
+
+    def test__not_found(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
